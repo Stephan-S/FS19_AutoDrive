7d6dd27f0b4daf9a8580ab14db1b4adcdee8c638
TyKonKet <matteoautieri@live.it>
2019-11-14 10:59:04 +0100

    Fixed #863

--- FS19_AutoDrive/scripts/AutoDrive.lua
+++ FS19_AutoDrive/scripts/AutoDrive.lua
@@ -1,5 +1,5 @@
 AutoDrive = {}
-AutoDrive.Version = "1.0.6.8-5"
+AutoDrive.Version = "1.0.6.8-6"
 AutoDrive.configChanged = false
 AutoDrive.handledRecalculation = true
 
@@ -572,5 +572,25 @@ AIVehicleUtil.driveInDirection = function(self, dt, steeringAngleLimit, accelera
 		WheelsUtil.updateWheelsPhysics(self, dt, self.lastSpeedReal * self.movingDirection, acc, not allowedToDrive, true)
 	end
 end
+
+Sprayer.registerOverwrittenFunctions =
+	Utils.appendedFunction(
+	Sprayer.registerOverwrittenFunctions,
+	function(vehicleType)
+		-- Work-around/fix for issue #863 ( thanks to DeckerMMIV )
+		-- Having a slurry tank with a spreading unit attached, then avoid having the AI automatically turn these on when FollowMe is active.
+		SpecializationUtil.registerOverwrittenFunction(
+			vehicleType,
+			"getIsAIActive",
+			function(self, superFunc)
+				local rootVehicle = self:getRootVehicle()
+				if nil ~= rootVehicle and rootVehicle.ad ~= nil and rootVehicle.ad.isActive then
+					return false -- "Hackish" work-around, in attempt at convincing Sprayer.LUA to NOT turn on
+				end
+				return superFunc(self)
+			end
+		)
+	end
+)
 
 addModEventListener(AutoDrive)
