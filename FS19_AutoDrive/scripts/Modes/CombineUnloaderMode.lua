9361e1421665123b21b4d4d92fa40215d4158356
aletheist <none>
2020-04-09 15:26:52 -0400

    Fixes for insufficient stand off from forage harvester.

--- FS19_AutoDrive/scripts/Modes/CombineUnloaderMode.lua
+++ FS19_AutoDrive/scripts/Modes/CombineUnloaderMode.lua
@@ -311,22 +311,25 @@ function CombineUnloaderMode:getSideChaseOffsetX()
 
     local sideChaseTermPipeIn = self.combine.sizeWidth/2 + 
                                 unloaderWidest + 
-                                headerExtra + pipeOffset
+                                headerExtra
     local sideChaseTermPipeOut = self.combine.sizeWidth/2 + 
-                                    (AutoDrive.getPipeLength(self.combine) + pipeOffset) + unloaderWidest/4    -- Some combines fold up their pipe so tight that targeting it could cause a collision.
+                                    AutoDrive.getPipeLength(self.combine) + unloaderWidest/4    -- Some combines fold up their pipe so tight that targeting it could cause a collision.
     -- So, choose the max between the two to avoid a collison
     local sideChaseTermX = math.max(sideChaseTermPipeIn, sideChaseTermPipeOut)
 
     local spec = self.combine.spec_pipe
     if self.combine:getIsBufferCombine() and not AutoDrive.isSugarcaneHarvester(self.combine) then
-        -- If it is a buffer combine, use the pipe in offset regardless
-        sideChaseTermX = sideChaseTermPipeIn
+        -- If it is a buffer combine, keep well away to allow for turns.
+        local turningRadius = AutoDrive.getDriverRadius(self.combine)
+        -- self.combine.sizeLength/2 there as a proxy for turningRadius if it turns out to be
+        -- some silly value.
+        sideChaseTermX = math.max(turningRadius, sideChaseTermPipeIn, self.combine.sizeLength/2 + headerExtra)
     elseif spec.currentState == spec.targetState and (spec.currentState == 2 or self.combine.typeName == "combineCutterFruitPreparer") then
         -- If the pipe is extended, though, target it regardless
         sideChaseTermX = sideChaseTermPipeOut
     end
 
-    return sideChaseTermX
+    return sideChaseTermX + pipeOffset
 end
 
 function CombineUnloaderMode:getDynamicSideChaseOffsetZ()
