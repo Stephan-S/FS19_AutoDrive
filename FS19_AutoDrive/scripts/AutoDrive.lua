2179427a034dfc4081d7bad9b7908697dae512ac
Axel32019 <52026009+Axel32019@users.noreply.github.com>
2020-06-24 17:26:26 +0200

    added Key binding for parking #1369

--- FS19_AutoDrive/scripts/AutoDrive.lua
+++ FS19_AutoDrive/scripts/AutoDrive.lua
@@ -78,7 +78,8 @@ AutoDrive.actions = {
 	{"ADNameDriver", false, 0},
 	{"ADRenameMapMarker", false, 0},
 	{"ADSwapTargets", false, 0},
-	{"AD_open_notification_history", false, 0}
+	{"AD_open_notification_history", false, 0},
+	{"ADParkVehicle", false, 0}
 }
 
 function AutoDrive:loadMap(name)
