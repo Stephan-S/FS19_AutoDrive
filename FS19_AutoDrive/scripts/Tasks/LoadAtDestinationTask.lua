c9f7f2bec2decd882c702187a40096cb1e8e6b48
Axel32019 <52026009+Axel32019@users.noreply.github.com>
2020-08-10 19:13:21 +0200

    keep trailer covers open at end of task

--- FS19_AutoDrive/scripts/Tasks/LoadAtDestinationTask.lua
+++ FS19_AutoDrive/scripts/Tasks/LoadAtDestinationTask.lua
@@ -56,7 +56,7 @@ function LoadAtDestinationTask:update(dt)
             --Check if we have actually loaded / tried to load
             AutoDrive.debugPrint(self.vehicle, AutoDrive.DC_VEHICLEINFO, "[AD] LoadAtDestinationTask:update isTargetReached")
             local trailers, _ = AutoDrive.getTrailersOf(self.vehicle, false)
-            AutoDrive.setTrailerCoverOpen(self.vehicle, trailers, false)
+            AutoDrive.setTrailerCoverOpen(self.vehicle, trailers, true)
             if (self.vehicle.ad.callBackFunction ~= nil or (g_courseplay ~= nil and self.vehicle.ad.stateModule:getStartCP_AIVE())) and self.vehicle.ad.stateModule:getMode() == AutoDrive.MODE_PICKUPANDDELIVER then
                 AutoDrive.debugPrint(self.vehicle, AutoDrive.DC_VEHICLEINFO, "[AD] LoadAtDestinationTask:update ERROR stopAutoDrive")
                 self.vehicle:stopAutoDrive()
