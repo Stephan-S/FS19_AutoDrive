9a675e68fe6c341850d727b24fa1d75e5c926248
Axel32019 <52026009+Axel32019@users.noreply.github.com>
2021-01-07 16:48:37 +0100

    added debugDrawBoundingBoxForVehicles

--- FS19_AutoDrive/scripts/Utils/CollisionDetectionUtils.lua
+++ FS19_AutoDrive/scripts/Utils/CollisionDetectionUtils.lua
@@ -153,3 +153,24 @@ function AutoDrive.getFrontToolLength(vehicle)
 
     return lengthOfFrontTool
 end
+
+function AutoDrive.debugDrawBoundingBoxForVehicles()
+    local vehicle = g_currentMission.controlledVehicle
+    if vehicle ~= nil and vehicle.getIsEntered ~= nil and vehicle:getIsEntered() then
+        local PosX, _, PosZ = getWorldTranslation(vehicle.components[1].node)
+        local maxDistance = AutoDrive.drawDistance
+        for _, otherVehicle in pairs(g_currentMission.vehicles) do
+            if otherVehicle ~= nil and otherVehicle.components ~= nil and otherVehicle.components[1].node ~= nil and otherVehicle.sizeWidth ~= nil and otherVehicle.sizeLength ~= nil and otherVehicle.rootNode ~= nil then
+                local x, _, z = getWorldTranslation(otherVehicle.components[1].node)
+                local distance = MathUtil.vector2Length(PosX - x, PosZ - z)
+                if distance < maxDistance then
+                    local boundingBox = AutoDrive.getBoundingBoxForVehicle(otherVehicle)
+                    ADDrawingManager:addLineTask(boundingBox[1].x, boundingBox[1].y, boundingBox[1].z, boundingBox[2].x, boundingBox[2].y, boundingBox[2].z, 1, 1, 0)
+                    ADDrawingManager:addLineTask(boundingBox[2].x, boundingBox[2].y, boundingBox[2].z, boundingBox[3].x, boundingBox[3].y, boundingBox[3].z, 1, 1, 0)
+                    ADDrawingManager:addLineTask(boundingBox[3].x, boundingBox[3].y, boundingBox[3].z, boundingBox[4].x, boundingBox[4].y, boundingBox[4].z, 1, 1, 0)
+                    ADDrawingManager:addLineTask(boundingBox[4].x, boundingBox[4].y, boundingBox[4].z, boundingBox[1].x, boundingBox[1].y, boundingBox[1].z, 1, 1, 0)
+                end
+            end
+        end
+    end
+end
