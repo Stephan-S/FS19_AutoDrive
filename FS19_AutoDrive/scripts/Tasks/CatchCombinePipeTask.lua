d4e0eb3b205e668ff27d61791e4f24a2d8c461d8
Axel32019 <52026009+Axel32019@users.noreply.github.com>
2021-01-21 17:59:07 +0100

    sendMessageOrNotification not required as tasks will continue

--- FS19_AutoDrive/scripts/Tasks/CatchCombinePipeTask.lua
+++ FS19_AutoDrive/scripts/Tasks/CatchCombinePipeTask.lua
@@ -47,7 +47,7 @@ function CatchCombinePipeTask:update(dt)
             if self.wayPoints == nil or #self.wayPoints < 1 then
                 --restart
                 self.vehicle.ad.modes[AutoDrive.MODE_UNLOAD]:notifyAboutFailedPathfinder()
-                AutoDriveMessageEvent.sendMessageOrNotification(self.vehicle, ADMessagesManager.messageTypes.WARN, "$l10n_AD_Driver_of; %s $l10n_AD_cannot_find_path; %s", 5000, self.vehicle.ad.stateModule:getName(), self.combine.ad.stateModule:getName())
+                --AutoDriveMessageEvent.sendMessageOrNotification(self.vehicle, ADMessagesManager.messageTypes.WARN, "$l10n_AD_Driver_of; %s $l10n_AD_cannot_find_path; %s", 5000, self.vehicle.ad.stateModule:getName(), self.combine.ad.stateModule:getName())
                 AutoDrive.debugPrint(self.vehicle, AutoDrive.DC_COMBINEINFO, "CatchCombinePipeTask:update - STATE_PATHPLANNING restarting path finder - with delay 10000")
                 self.state = CatchCombinePipeTask.STATE_DELAY_PATHPLANNING
             else
