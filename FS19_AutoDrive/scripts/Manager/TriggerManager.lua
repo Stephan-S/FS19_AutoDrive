a555a6af2368241658cbec8d4f0b88c9c75c9ed2
Axel32019 <52026009+Axel32019@users.noreply.github.com>
2021-02-05 19:31:40 +0100

    include GC factory trigger in getRefuelTriggers

--- FS19_AutoDrive/scripts/Manager/TriggerManager.lua
+++ FS19_AutoDrive/scripts/Manager/TriggerManager.lua
@@ -244,29 +244,27 @@ function ADTriggerManager.getRefuelTriggers(vehicle)
 
         for _, trigger in pairs(ADTriggerManager.getLoadTriggers()) do
             --loadTriggers
-            if trigger.source ~= nil and trigger.source.providedFillTypes ~= nil and trigger.source.providedFillTypes[fillType] then
-                -- vanilla LoadingStation
-                local fillLevels = {}
-                if trigger.source ~= nil and trigger.source.getAllFillLevels ~= nil then
-                    fillLevels, _ = trigger.source:getAllFillLevels(vehicle:getOwnerFarmId())
-                end
-                -- GC trigger
-                local gcFillLevels = {}
-                if trigger.source ~= nil and trigger.source.getAllProvidedFillLevels ~= nil then
-                    gcFillLevels, _ = trigger.source:getAllProvidedFillLevels(vehicle:getOwnerFarmId(), trigger.managerId)
-                end
-                if table.getn(fillLevels) == 0 and table.getn(gcFillLevels) == 0 and trigger.source ~= nil and trigger.source.gcId ~= nil and trigger.source.fillLevels ~= nil then
-                    for index, fillLevel in pairs(trigger.source.fillLevels) do
-                        if fillLevel ~= nil and fillLevel[1] ~= nil then
-                            fillLevels[index] = fillLevel[1]
-                        end
+            -- vanilla LoadingStation
+            local fillLevels = {}
+            if trigger.source ~= nil and trigger.source.getAllFillLevels ~= nil then
+                fillLevels, _ = trigger.source:getAllFillLevels(vehicle:getOwnerFarmId())
+            end
+            -- GC trigger
+            local gcFillLevels = {}
+            if trigger.source ~= nil and trigger.source.getAllProvidedFillLevels ~= nil then
+                gcFillLevels, _ = trigger.source:getAllProvidedFillLevels(vehicle:getOwnerFarmId(), trigger.managerId)
+            end
+            if table.getn(fillLevels) == 0 and table.getn(gcFillLevels) == 0 and trigger.source ~= nil and trigger.source.gcId ~= nil and trigger.source.fillLevels ~= nil then
+                for index, fillLevel in pairs(trigger.source.fillLevels) do
+                    if fillLevel ~= nil and fillLevel[1] ~= nil then
+                        fillLevels[index] = fillLevel[1]
                     end
                 end
-                local hasCapacity = (fillLevels[fillType] ~= nil and fillLevels[fillType] > 0) or (gcFillLevels[fillType] ~= nil and gcFillLevels[fillType] > 0)
+            end
+            local hasCapacity = (fillLevels[fillType] ~= nil and fillLevels[fillType] > 0) or (gcFillLevels[fillType] ~= nil and gcFillLevels[fillType] > 0)
 
-                if hasCapacity then
-                    table.insert(refuelTriggers, trigger)
-                end
+            if hasCapacity then
+                table.insert(refuelTriggers, trigger)
             end
         end
     end
