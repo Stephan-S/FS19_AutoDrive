893a4d6a76e892044c8c8fc24ee3d7a277a170c1
jala15 <52028079+jala15@users.noreply.github.com>
2021-04-21 00:36:03 +0200

    Fix #1879
    Follow combine tweaks

--- FS19_AutoDrive/scripts/Modules/SpecialDrivingModule.lua
+++ FS19_AutoDrive/scripts/Modules/SpecialDrivingModule.lua
@@ -131,12 +131,14 @@ function ADSpecialDrivingModule:driveToPoint(dt, point, maxFollowSpeed, checkDyn
     local x, y, z = getWorldTranslation(self.vehicle.components[1].node)
     self.distanceToChasePos = MathUtil.vector2Length(x - point.x, z - point.z)
 
-    if self.distanceToChasePos < 1.75 then
-        speed = maxFollowSpeed * 1
+    if self.distanceToChasePos < 0.5 then
+        speed = maxFollowSpeed + self.distanceToChasePos * 0.5
+    elseif self.distanceToChasePos < 2 then
+		speed = math.max(maxFollowSpeed + self.distanceToChasePos * 0.5, self.distanceToChasePos * 3)
     elseif self.distanceToChasePos < 7 then
-        speed = maxFollowSpeed + self.distanceToChasePos * 1.4
+        speed = math.max(maxFollowSpeed, 6) + self.distanceToChasePos * 1
     elseif self.distanceToChasePos < 20 then
-        speed = maxFollowSpeed + self.distanceToChasePos * 2
+        speed = math.max(maxFollowSpeed, 6) + (self.distanceToChasePos - 3.5) * 2
     end
 
     --print("Targetspeed: " .. speed .. " distance: " .. self.distanceToChasePos .. " maxFollowSpeed: " .. maxFollowSpeed)
