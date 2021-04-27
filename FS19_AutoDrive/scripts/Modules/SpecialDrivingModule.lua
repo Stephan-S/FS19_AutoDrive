8eb0e35bff4b45f57672c4a8df0f203949c86ad0
Axel32019 <52026009+Axel32019@users.noreply.github.com>
2021-04-27 17:11:57 +0200

    reduced the harvester follow distance with same speed to 0.5m

--- FS19_AutoDrive/scripts/Modules/SpecialDrivingModule.lua
+++ FS19_AutoDrive/scripts/Modules/SpecialDrivingModule.lua
@@ -131,7 +131,7 @@ function ADSpecialDrivingModule:driveToPoint(dt, point, maxFollowSpeed, checkDyn
     local x, y, z = getWorldTranslation(self.vehicle.components[1].node)
     self.distanceToChasePos = MathUtil.vector2Length(x - point.x, z - point.z)
 
-    if self.distanceToChasePos < 1.75 then
+    if self.distanceToChasePos < 0.5 then
         speed = maxFollowSpeed * 1
     elseif self.distanceToChasePos < 7 then
         speed = maxFollowSpeed + self.distanceToChasePos * 1.4
