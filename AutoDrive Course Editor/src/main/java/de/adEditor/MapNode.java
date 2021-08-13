91ef741d340fee88d495c6ddf2e1c032c8b0893a
KillBait <KillBait@users.noreply.github.com>
2021-08-13 12:46:08 +0100

    Update MapNode.java
    added flag to mapnode

--- AutoDrive Course Editor/src/main/java/de/adEditor/MapNode.java
+++ AutoDrive Course Editor/src/main/java/de/adEditor/MapNode.java
@@ -7,15 +7,16 @@ public class MapNode {
     public LinkedList<MapNode> incoming;
     public LinkedList<MapNode> outgoing;
     public double x, y, z;
-    public int id;
+    public int id, flag;
 
-    public MapNode(int id, double x, double y, double z) {
+    public MapNode(int id, double x, double y, double z, int flag) {
         this.id = id;
         this.x = x;
         this.y = y;
         this.z = z;
         this.incoming = new LinkedList<>();
         this.outgoing = new LinkedList<>();
+        this.flag = flag;
     }
 
 
